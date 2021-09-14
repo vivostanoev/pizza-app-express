aab8ff34f0db6c1e4458206e97ab0b7a0f0f72a1
vivostanoev <vivostanoev@gmail.com>
2021-09-14 16:15:08 +0300

    last changes

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ const app = express();
 const {foods} = require('./Data/FoodData')
 const cors = require('cors');
 const {MongoClient} = require('mongodb');
-const uri = "";
+const uri = "mongodb+srv://viliyan:viliyan@viliyan.zql7y.mongodb.net/pizza?retryWrites=true&w=majority";
 app.use(cors());
 app.use(express.json());
 app.use(express.static('img'));
-let finalOrder = [];
-let users = [];
 let foodItems = [];
 
 async function insertUser(item){
@@ -19,6 +17,19 @@ async function insertUser(item){
 
   const db = await client.db('pizza');
   await db.collection('users').insertOne(item); 
+  
+  await client.close();
+}
+
+
+async function insertOrder(order){
+  const client = await MongoClient.connect(uri, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+});
+
+  const db = await client.db('pizza');
+  await db.collection('orders').insertOne(order); 
   
   await client.close();
 }
@@ -43,8 +54,25 @@ async function getUser(item){
 });
 
   const db = await client.db('pizza');
-  users = await db.collection('users').find({"username": item.username}).toArray();
+  let users = await db.collection('users').find({"username": item.username}).toArray();
   await client.close();
+
+  return users;
+}
+
+async function getOrders(username){
+  const client = await MongoClient.connect(uri, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+});
+
+
+  const db = await client.db('pizza');
+  let finalOrders = await db.collection('orders').find({"user": username}).toArray();
+
+  await client.close();
+
+  return finalOrders;
 }
 
 app.get('/', function (req, res) {
@@ -56,16 +84,29 @@ app.post('/api/auth', function (req, res){
       res.status(400).json({message:'Pleae fill all usercredantials'});
     }
     else{
-    getUser(req.body);
+
+    let users = getUser(req.body);
     
-    users.forEach((user) => {
-        if(user.password == req.body.password)
+    let isUserExist = false;
+
+    users.then(function(result){
+        for (let index = 0; index < result.length; index++) {
+          const element = result[index];
+          if(element.password === req.body.password)
+          {
+             isUserExist = true;
+             console.log("here" + isUserExist);
+          }
+        }
+
+        if(isUserExist)
         {
-         res.status(200).json("Successful Login!!!");
+          res.status(200).json({status: 200, message: "Successfull"});
+        }
+        else{
+         res.status(404).json({status: 1, message: "Wrong email address or password"});
         }
     });
-
-     res.status(404).json({status: 1, message: "Wrong email address or password"});
     }
 });
 
@@ -75,34 +116,43 @@ app.post('/api/post/user', function (req, res){
      return;
   }
   else{
-  getUser(req.body);
-  users.forEach((user) => {
-      if(user.username == req.body.username)
-      {
-        res.status(401).json({message:"Email address already exist"});
-      }
+  let users = getUser(req.body);
+  
+  users.then(function(result){
+    if(result.length === 0)
+    { 
+      insertUser(req.body);
+      res.status(200).json({message:"Successful register"});
+    }
+    else
+    {
+      res.status(401).json({message:"Email address already exist"});
+    }
   });
-
-  insertUser(req.body);
-  res.status(200).json({message:"Successful register"});
   }
 });
 
 app.post('/api/post/order', function (req, res){
-  if(req.body[0].length===0)
+  if(req.body.length===0)
   {
     res.status(400).json({message:'Please fill your order before proccess'});
   }
-  transferOrders(req.body);
+  else
+  {
+    transferOrders(req.body);
+  }
 });
 
 app.get('/api/get/orders/:user', function (req, res){
-  let orders = getAllOrdersByUsername(req.params.user);
+  let orders = getOrders(req.params.user);
+  
+  orders.then(function(result){
+    res.send(result)
+    });
+
   if(!orders || orders.length==0){
     res.status(404).send("Not found");
   }
-
-  res.json(orders).send(); 
 });
 
 app.get('/api/get/foods', function (req, res){
@@ -110,22 +160,11 @@ app.get('/api/get/foods', function (req, res){
   res.json(foods(foodItems)).send(); 
 });
 
-function getAllOrdersByUsername(username){
-  return finalOrder.filter(o => o.user === username);
-}
-
 function transferOrders(orders){
-  orders[0].forEach(element => {
-      let order = new Object();
-      order.name = element.name;
-      order.toppings = element.toppings.filter(t => t.checked).map(topping => topping.name).join(", ");
-      let subtotal = (element.price * element.quantity) + order.toppings.length * 0.6;
-      order.price = subtotal + subtotal*0.1;
-      order.user = orders[1].user;
-      order.status = "Draft";
-      order.id = getAllOrdersByUsername(orders[1].user).length + 1;
-      finalOrder.push(order);
-  });
+orders.date = new Date().toLocaleString();
+orders.status = "Draft";
+
+insertOrder(orders);
 }
 
 app.listen(5000);
